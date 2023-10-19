9b85f4f364abf9c77af109fa20aafaaea9e43d3a
Kéwan Bernier <mail@kewan.fr>
2023-10-19 08:25:28 +0200

    Jeu du pendu

--- Projet4 - Pendu.py
+++ Projet4 - Pendu.py
@@ -1,9 +1,11 @@
 
 class Pendu:
     def __init__(self):
-        # self.mot_secret = input("Votre mot: ")
+        self.mot_secret = input("Votre mot: ").upper()
+        # print(self.mot_secret)
         self.compteur_erreurs = 0
-        self.mot_devoile = ''
+        self.mot_devoile = ["_"] * len(self.mot_secret)
+        self.affiche_mot()
 
     def affichage_dessin(self):
         etapes = {
@@ -21,20 +23,48 @@ class Pendu:
         if 0 <= self.compteur_erreurs <= 9:
             print(etapes[self.compteur_erreurs])
 
-    def affiche2(self):
-        pass
+    def affiche_mot(self):
+        for l in self.mot_devoile:
+            print(l + " ", end="")
+        print("\n")
 
-    def verifie(self):
-        pass
+    def verifie(self, lettre):
+        if lettre in self.mot_devoile:
+            return True
+        return False
 
     def tour(self):
-        pass
+        lettre = input("Votre lettre: ").upper()
+        if self.verifie(lettre):
+            self.affiche_mot()
+            self.compteur_erreurs += 1
+            return print("Lettre déjà trouvée")
+        if lettre in self.mot_secret:
+            print("Oui")
+            for i in range(len(self.mot_secret)):
+                if self.mot_secret[i] == lettre:
+                    self.mot_devoile[i] = lettre
+        else:
+            print("Non")
+            self.compteur_erreurs += 1
+        self.affiche_mot()
+
 
     def partie(self):
-        pass
+        continuer = True
+        while self.compteur_erreurs < 9 and continuer:
+            self.tour()
+            self.affichage_dessin()
+            if "_" not in self.mot_devoile:
+                print("Gagné !")
+                return
+        if self.compteur_erreurs >= 9:
+            print("Perdu !")
         
 jeu = Pendu()
 # jeu.partie()
 # jeu.affichage_dessin()
 # jeu.compteur_erreurs += 9
 # jeu.affichage_dessin()
+jeu.partie()
+# jeu.tour()
